d5513e12a9abb6eb27a5b59de400b57ad6391b7e
HMSI <HMSI@GKV.LOCAL>
2018-04-20 10:26:10 +0700

    2018/04/20

---
+++ index.php
@@ -0,0 +1,583 @@
+<?php
+/********************************
+Simple PHP File Manager
+Copyright John Campbell (jcampbell1)
+
+Liscense: MIT
+********************************/
+
+//Disable error report for undefined superglobals
+error_reporting( error_reporting() & ~E_NOTICE );
+
+//Security options
+$allow_delete = true; // Set to false to disable delete button and delete POST request.
+$allow_create_folder = true; // Set to false to disable folder creation
+$allow_upload = true; // Set to true to allow upload files
+$allow_direct_link = true; // Set to false to only allow downloads and not direct link
+
+$disallowed_extensions = [];  // must be an array. Extensions disallowed to be uploaded
+
+$hidden_extensions = []; // must be an array of lowercase file extensions. Extensions hidden in directory index
+
+$PASSWORD = 'Hoangsi790si';  // Set the password, to access the file manager... (optional)
+
+if($PASSWORD) {
+
+	session_start();
+	if(!$_SESSION['_sfm_allowed']) {
+		// sha1, and random bytes to thwart timing attacks.  Not meant as secure hashing.
+		$t = bin2hex(openssl_random_pseudo_bytes(10));	
+		if($_POST['p'] && sha1($t.$_POST['p']) === sha1($t.$PASSWORD)) {
+			$_SESSION['_sfm_allowed'] = true;
+			header('Location: ?');
+		}
+		echo '<!DOCTYPE html>
+		<html lang="en">
+		
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+			<style>
+				body {
+					background: #222;
+					position: fixed;
+					width: 100%;
+					height: 100%;
+					text-align: center;
+				}
+		
+				#login {
+					width: 279px;
+					display: inline-block;
+					position: relative;
+					top: 50%;
+					-webkit-transform: translateY(-50%);
+					-ms-transform: translateY(-50%);
+					transform: translateY(-50%);
+				}
+		
+				#password {
+					border: none;
+					background: #333;
+					outline: none;
+					color: #fff;
+				}
+			</style>
+			<title>Login</title>
+		</head>
+		
+		<body>
+		
+			<form action="?" id="login" method="POST">
+				<input type="password" class="form-control" name="p" id="password" autocomplete="off" placeholder="••••••••">
+		
+			</form>
+		
+		</body>
+		
+		</html>'; 
+		exit;
+	}
+}
+
+// must be in UTF-8 or `basename` doesn't work
+setlocale(LC_ALL,'en_US.UTF-8');
+
+$tmp_dir = dirname($_SERVER['SCRIPT_FILENAME']);
+if(DIRECTORY_SEPARATOR==='\\') $tmp_dir = str_replace('/',DIRECTORY_SEPARATOR,$tmp_dir);
+$tmp = get_absolute_path($tmp_dir . '/' .$_REQUEST['file']);
+
+if($tmp === false)
+	err(404,'File or Directory Not Found');
+if(substr($tmp, 0,strlen($tmp_dir)) !== $tmp_dir)
+	err(403,"Forbidden");
+if(strpos($_REQUEST['file'], DIRECTORY_SEPARATOR) === 0) 
+	err(403,"Forbidden");
+
+
+if(!$_COOKIE['_sfm_xsrf'])
+	setcookie('_sfm_xsrf',bin2hex(openssl_random_pseudo_bytes(16)));
+if($_POST) {
+	if($_COOKIE['_sfm_xsrf'] !== $_POST['xsrf'] || !$_POST['xsrf'])
+		err(403,"XSRF Failure");
+}
+
+$file = $_REQUEST['file'] ?: '.';
+if($_GET['do'] == 'list') {
+	if (is_dir($file)) {
+		$directory = $file;
+		$result = [];
+		$files = array_diff(scandir($directory), ['.','..']);
+	    foreach($files as $entry) if($entry !== basename(__FILE__) && !in_array(strtolower(pathinfo($entry, PATHINFO_EXTENSION)), $hidden_extensions)) {
+    		$i = $directory . '/' . $entry;
+	    	$stat = stat($i);
+	        $result[] = [
+	        	'mtime' => $stat['mtime'],
+	        	'size' => $stat['size'],
+	        	'name' => basename($i),
+	        	'path' => preg_replace('@^\./@', '', $i),
+	        	'is_dir' => is_dir($i),
+	        	'is_deleteable' => $allow_delete && ((!is_dir($i) && is_writable($directory)) ||
+                                                           (is_dir($i) && is_writable($directory) && is_recursively_deleteable($i))),
+	        	'is_readable' => is_readable($i),
+	        	'is_writable' => is_writable($i),
+	        	'is_executable' => is_executable($i),
+	        ];
+	    }
+	} else {
+		err(412,"Not a Directory");
+	}
+	echo json_encode(['success' => true, 'is_writable' => is_writable($file), 'results' =>$result]);
+	exit;
+} elseif ($_POST['do'] == 'delete') {
+	if($allow_delete) {
+		rmrf($file);
+	}
+	exit;
+} elseif ($_POST['do'] == 'mkdir' && $allow_create_folder== true) {
+	// don't allow actions outside root. we also filter out slashes to catch args like './../outside'
+	
+	$dir = $_POST['name'];
+	$dir = str_replace('/', '', $dir);
+	if(substr($dir, 0, 2) === '..')
+	    exit;
+	chdir($file);
+	@mkdir($_POST['name']);
+	exit;
+} elseif ($_POST['do'] == 'upload' && $allow_upload == true) {
+	var_dump($_POST);
+	var_dump($_FILES);
+	var_dump($_FILES['file_data']['tmp_name']);
+	foreach($disallowed_extensions as $ext) 
+		if(preg_match(sprintf('/\.%s$/',preg_quote($ext)), $_FILES['file_data']['name'])) 
+			err(403,"Files of this type are not allowed.");
+
+	var_dump(move_uploaded_file($_FILES['file_data']['tmp_name'], $file.'/'.$_FILES['file_data']['name']));
+	exit;
+} elseif ($_GET['do'] == 'download') {
+	$filename = basename($file);
+	header('Content-Type: ' . mime_content_type($file));
+	header('Content-Length: '. filesize($file));
+	header(sprintf('Content-Disposition: attachment; filename=%s',
+		strpos('MSIE',$_SERVER['HTTP_REFERER']) ? rawurlencode($filename) : "\"$filename\"" ));
+	ob_flush();
+	readfile($file);
+	exit;
+}
+function rmrf($dir) {
+	if(is_dir($dir)) {
+		$files = array_diff(scandir($dir), ['.','..']);
+		foreach ($files as $file)
+			rmrf("$dir/$file");
+		rmdir($dir);
+	} else {
+		unlink($dir);
+	}
+}
+function is_recursively_deleteable($d) {
+	$stack = [$d];
+	while($dir = array_pop($stack)) {
+		if(!is_readable($dir) || !is_writable($dir)) 
+			return false;
+		$files = array_diff(scandir($dir), ['.','..']);
+		foreach($files as $file) if(is_dir($file)) {
+			$stack[] = "$dir/$file";
+		}
+	}
+	return true;
+}
+
+// from: http://php.net/manual/en/function.realpath.php#84012
+function get_absolute_path($path) {
+        $path = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
+        $parts = explode(DIRECTORY_SEPARATOR, $path);
+        $absolutes = [];
+        foreach ($parts as $part) {
+            if ('.' == $part) continue;
+            if ('..' == $part) {
+                array_pop($absolutes);
+            } else {
+                $absolutes[] = $part;
+            }
+        }
+        return implode(DIRECTORY_SEPARATOR, $absolutes);
+    }
+
+function err($code,$msg) {
+	http_response_code($code);
+	echo json_encode(['error' => ['code'=>intval($code), 'msg' => $msg]]);
+	exit;
+}
+
+function asBytes($ini_v) {
+	$ini_v = trim($ini_v);
+	$s = ['g'=> 1<<30, 'm' => 1<<20, 'k' => 1<<10];
+	return intval($ini_v) * ($s[strtolower(substr($ini_v,-1))] ?: 1);
+}
+$MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('upload_max_filesize')));
+?>
+<!DOCTYPE html>
+<html lang="vi"><head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+<title>Sĩ Hoàng</title>
+<style>
+body {margin:0;padding: 15px 0;padding-top: 220px;}
+th {font-weight: normal; color: #1F75CC; background-color: #F0F9FF; padding:.5em 1em .5em .2em; 
+	text-align: left;cursor:pointer;user-select: none;}
+th .indicator {margin-left: 6px }
+thead {border-top: 1px solid #82CFFA; border-bottom: 1px solid #96C4EA;border-left: 1px solid #E7F2FB;
+	border-right: 1px solid #E7F2FB; }
+header {position: fixed;
+    top: 0;
+    left: 0;
+	background: #fff;
+    z-index: 99;
+	/* padding-bottom: 15px; */
+	-webkit-box-shadow: 0px 2px 5px -1px rgba(0, 0, 0, .2);
+    box-shadow: 0px 2px 5px -1px rgba(0, 0, 0, .2);
+    right: 0;}
+
+#top {padding: 15px;
+    background: #2f2f2f;
+    }
+#mkdir {display:inline-block;padding-bottom:5px;}
+label { display:block; font-size:11px; color:#555;}
+#file_drop_target {width:500px; padding:12px 0; border: 4px dashed #ccc;font-size:12px;color:#ccc; cursor: pointer;
+	text-align: center;margin-right:20px;}
+#file_drop_target.drag_over {border: 4px dashed #96C4EA; color: #96C4EA;}
+#upload_progress {padding: 4px 0;}
+#upload_progress .error {color:#a00;}
+#upload_progress > div { padding:3px 0;}
+.no_write #mkdir, .no_write #file_drop_target {display: none}
+.progress_track {    display: inline-block;
+    width: 200px;
+    height: 12px;
+    border: 1px solid #bfbfbf;
+    margin: 0 4px 0 10px;
+    border-radius: 4px;}
+.progress {background-color: #82CFFA;height:10px; }
+footer {font-size:11px; color:#bbbbc5; padding:4em 0 0;text-align: left;}
+footer a, footer a:visited {color:#bbbbc5;}
+#breadcrumb { display:inline-block;float:left; width: 100%; margin-top:10px; margin-bottom:0px;}
+#folder_actions {width: 50%;float:right;}
+/* a, a:visited { color:#00c; text-decoration: none} */
+a:hover {text-decoration: underline}
+.sort_hide{ display:none;}
+table {border-collapse: collapse;width:100%;}
+thead {max-width: 1024px}
+/* td { padding:.2em 1em .2em .2em; border-bottom:1px solid #def;height:30px; font-size:12px;white-space: nowrap;} */
+td.first {font-size:14px;white-space: normal;}
+td.empty { color:#777; font-style: italic; text-align: center;}
+.is_dir .size {color:transparent;font-size:0;}
+.is_dir .size:before {content: "--"; font-size:14px;color:#333;}
+.is_dir .download{visibility: hidden}
+
+
+.fileuploadname, .size, .progress_track  {display: inline-block;
+    vertical-align: middle;}
+
+.progress_track {}
+
+
+a.delete {display:inline-block;
+
+
+
+
+
+
+
+background: url('https://hoangsi.com/hoangsi.com/del.svg') left center no-repeat; background-size: 12px;
+	padding:4px 0 4px 20px;
+
+
+
+}
+.name {
+	background: url('https://hoangsi.com/hoangsi.com/file.svg') left center no-repeat; background-size: 20px;
+	padding:15px 0 10px 40px;
+}
+.is_dir .name {
+	
+	background: url('https://hoangsi.com/hoangsi.com/folder.svg') left center no-repeat; background-size: 20px;
+	
+	padding:15px 0 10px 40px;
+}
+.download {
+	background: url('https://hoangsi.com/hoangsi.com/download.svg') left center no-repeat; background-size: 15px;
+	padding:4px 30px 4px 22px;
+}
+</style>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script>
+(function($){
+	$.fn.tablesorter = function() {
+		var $table = this;
+		this.find('th').click(function() {
+			var idx = $(this).index();
+			var direction = $(this).hasClass('sort_asc');
+			$table.tablesortby(idx,direction);
+		});
+		return this;
+	};
+	$.fn.tablesortby = function(idx,direction) {
+		var $rows = this.find('tbody tr');
+		function elementToVal(a) {
+			var $a_elem = $(a).find('td:nth-child('+(idx+1)+')');
+			var a_val = $a_elem.attr('data-sort') || $a_elem.text();
+			return (a_val == parseInt(a_val) ? parseInt(a_val) : a_val);
+		}
+		$rows.sort(function(a,b){
+			var a_val = elementToVal(a), b_val = elementToVal(b);
+			return (a_val > b_val ? 1 : (a_val == b_val ? 0 : -1)) * (direction ? 1 : -1);
+		})
+		this.find('th').removeClass('sort_asc sort_desc');
+		$(this).find('thead th:nth-child('+(idx+1)+')').addClass(direction ? 'sort_desc' : 'sort_asc');
+		for(var i =0;i<$rows.length;i++)
+			this.append($rows[i]);
+		this.settablesortmarkers();
+		return this;
+	}
+	$.fn.retablesort = function() {
+		var $e = this.find('thead th.sort_asc, thead th.sort_desc');
+		if($e.length)
+			this.tablesortby($e.index(), $e.hasClass('sort_desc') );
+		
+		return this;
+	}
+	$.fn.settablesortmarkers = function() {
+		this.find('thead th span.indicator').remove();
+		this.find('thead th.sort_asc').append('<span class="indicator">&darr;<span>');
+		this.find('thead th.sort_desc').append('<span class="indicator">&uarr;<span>');
+		return this;
+	}
+})(jQuery);
+$(function(){
+	var XSRF = (document.cookie.match('(^|; )_sfm_xsrf=([^;]*)')||0)[2];
+	var MAX_UPLOAD_SIZE = <?php echo $MAX_UPLOAD_SIZE ?>;
+	var $tbody = $('#list');
+	$(window).bind('hashchange',list).trigger('hashchange');
+	$('#table').tablesorter();
+	
+	$('.delete').live('click',function(data) {
+		if(confirm('Chắc chắn xóa?')) {
+		$.post("",{'do':'delete',file:$(this).attr('data-file'),xsrf:XSRF},function(response){
+			list();
+		},'json');
+		return false;
+	}
+	});
+
+	$('#mkdir').submit(function(e) {
+		var hashval = window.location.hash.substr(1),
+			$dir = $(this).find('[name=name]');
+		e.preventDefault();
+		$dir.val().length && $.post('?',{'do':'mkdir',name:$dir.val(),xsrf:XSRF,file:hashval},function(data){
+			list();
+		},'json');
+		$dir.val('');
+		return false;
+	});
+<?php if($allow_upload == true): ?>
+	// file upload stuff
+	$('#file_drop_target').bind('dragover',function(){
+		$(this).addClass('drag_over');
+		return false;
+	}).bind('dragend',function(){
+		$(this).removeClass('drag_over');
+		return false;
+	}).bind('drop',function(e){
+		e.preventDefault();
+		var files = e.originalEvent.dataTransfer.files;
+		$.each(files,function(k,file) {
+			uploadFile(file);
+		});
+		$(this).removeClass('drag_over');
+	});
+	$('input[type=file]').change(function(e) {
+		e.preventDefault();
+		$.each(this.files,function(k,file) {
+			uploadFile(file);
+		});
+	});
+
+
+
+
+	function uploadFile(file) {
+		var folder = window.location.hash.substr(1);
+
+		if(file.size > MAX_UPLOAD_SIZE) {
+			var $error_row = renderFileSizeErrorRow(file,folder);
+			$('#upload_progress').append($error_row);
+			window.setTimeout(function(){$error_row.fadeOut();},50000);
+			return false;
+		}
+		
+		var $row = renderFileUploadRow(file,folder);
+		$('#upload_progress').append($row);
+		var fd = new FormData();
+		fd.append('file_data',file);
+		fd.append('file',folder);
+		fd.append('xsrf',XSRF);
+		fd.append('do','upload');
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', '?');
+		xhr.onload = function() {
+			$row.remove();
+    		list();
+  		};
+		xhr.upload.onprogress = function(e){
+			if(e.lengthComputable) {
+				$row.find('.progress').css('width',(e.loaded/e.total*100 | 0)+'%' );
+			}
+		};
+	    xhr.send(fd);
+	}
+	function renderFileUploadRow(file,folder) {
+		return $row = $('<div/>')
+			.append( $('<span class="fileuploadname" />').text( (folder ? folder+'/':'')+file.name))
+			.append( $('<div class="progress_track"><div class="progress"></div></div>')  )
+			.append( $('<span class="size" />').text(formatFileSize(file.size)) )
+	};
+	function renderFileSizeErrorRow(file,folder) {
+		return $row = $('<div class="error" />')
+			.append( $('<span class="fileuploadname" />').text( 'Error: ' + (folder ? folder+'/':'')+file.name))
+			.append( $('<span/>').html(' file size - <b>' + formatFileSize(file.size) + '</b>'
+				+' exceeds max upload size of <b>' + formatFileSize(MAX_UPLOAD_SIZE) + '</b>')  );
+	}
+<?php endif; ?>
+	function list() {
+		var hashval = window.location.hash.substr(1);
+		$.get('?',{'do':'list','file':hashval},function(data) {
+			$tbody.empty();
+			$('#breadcrumb').empty().html(renderBreadcrumbs(hashval));
+			if(data.success) {
+				$.each(data.results,function(k,v){
+					$tbody.append(renderFileRow(v));
+				});
+				!data.results.length && $tbody.append('<tr><td class="empty" colspan=6>Thư mục trống</td></tr>')
+				data.is_writable ? $('body').removeClass('no_write') : $('body').addClass('no_write');
+			} else {
+				console.warn(data.error.msg);
+			}
+			$('#table').retablesort();
+		},'json');
+	}
+	function renderFileRow(data) {
+		var $link = $('<a class="name" />')
+			.attr('href', data.is_dir ? '#' + data.path : '?do=download&file='+encodeURIComponent(data.path))
+			.text(data.name);
+		var allow_direct_link = <?php echo $allow_direct_link?'true':'false'; ?>;
+        	if (!data.is_dir && !allow_direct_link)  $link.css('pointer-events','none');
+		var $dl_link = $('<a/>').attr('href','?do=download&file='+encodeURIComponent(data.path))
+			.addClass('download').text('Tải xuống');
+		var $delete_link = $('<a href="#" />').attr('data-file',data.path).addClass('delete').text('Xóa');
+		var perms = [];
+		if(data.is_readable) perms.push('read');
+		if(data.is_writable) perms.push('write');
+		if(data.is_executable) perms.push('exec');
+		var $html = $('<tr />')
+			.addClass(data.is_dir ? 'is_dir' : '')
+			.append( $('<td class="first" />').append($link) )
+			.append( $('<td/>').attr('data-sort',data.is_dir ? -1 : data.size)
+				.html($('<span class="size" />').text(formatFileSize(data.size))) ) 
+			.append( $('<td/>').attr('data-sort',data.mtime).text(formatTimestamp(data.mtime)) )
+			.append( $('<td/>').text(perms.join('+')) )
+			.append( $('<td/>').html('<span class="label label-success">http://download.hoangsi.com/'+encodeURIComponent(data.path)))
+			.append( $('<td/>').append($dl_link).append( data.is_deleteable ? $delete_link : '') )
+		return $html;
+	}
+	function renderBreadcrumbs(path) {
+		var base = "",
+			$html = $('<div/>').append( $('<a href=#><i class="fa fa-home" aria-hidden="true"></i></a></div>') );
+		$.each(path.split('/'),function(k,v){
+			if(v) {
+				$html.append( $('<span/>').text(' / ') )
+					.append( $('<a/>').attr('href','#'+base+v).text(v) );
+				base += v + '/';
+			}
+		});
+		return $html;
+	}
+	function formatTimestamp(unix_timestamp) {
+		var m = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+		var d = new Date(unix_timestamp*1000);
+		// return [m[d.getMonth()],'/',d.getDate(),'/',d.getFullYear()," ",
+		return [d.getDate(),'/',m[d.getMonth()],'/',d.getFullYear()," ",
+			(d.getHours() % 12 || 12),":",(d.getMinutes() < 10 ? '0' : '')+d.getMinutes(),
+			" ",d.getHours() >= 12 ? 'PM' : 'AM'].join('');
+	}
+	function formatFileSize(bytes) {
+		var s = ['bytes', 'KB','MB','GB','TB','PB','EB'];
+		for(var pos = 0;bytes >= 1000; pos++,bytes /= 1024);
+		var d = Math.round(bytes*10);
+		return pos ? [parseInt(d/10),".",d%10," ",s[pos]].join('') : bytes + ' bytes';
+	}
+})
+
+</script>
+</head><body>
+<div class="container-fluid">
+
+<header> 
+
+<div id="top">
+   <?php if($allow_upload == true): ?>
+   <div class="col-xs-12">
+	<form action="?" method="post" id="mkdir" />
+
+<label class="input-group"  style="width:500px;">
+      <input type="text" class="form-control" placeholder="Tạo thư mục mới..." name="name" style="font-weight:lighter;">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button"><i class="fa fa-folder-open" aria-hidden="true"></i></button>
+      </span>
+    </label>
+
+
+		
+	</form>
+</div>
+   <?php endif; ?>
+   <?php if($allow_upload == true): ?>
+   <div class="col-xs-12">
+	<label id="file_drop_target">
+		Kéo thả File tại đây để Upload
+		<input type="file" multiple style="display:none;"/>
+	</label>
+	</div>
+   <?php endif; ?>
+   <div class="clearfix"></div>
+   </div>
+	
+   <div class="col-xs-12">
+<div id="breadcrumb" class="breadcrumb">&nbsp;</div>
+ <div class="clearfix"></div>
+<div id="upload_progress" ></div>
+</div>
+
+
+ <div class="clearfix"></div>
+
+
+</header>
+
+
+
+<table id="table" class="table table-striped"><thead><tr>
+	<th>Tên file</th>
+	<th>Kích cỡ</th>
+	<th>Ngày tải lên</th>
+	<th>Quyền</th>
+	<th>Url</th>
+	<th style="width: 220px;">Tùy chọn</th>
+</tr></thead><tbody id="list">
+
+</tbody></table>
+</div>
+<p style="position:fixed;left:0;width:100%;bottom:0;background:#f1f1f1;padding:10px 0; margin:0;font-size:12px;" class="text-center">1 sản phẩm của <a href="https://hoangsi.com/" target="_blank">Hoang Si</a></p>
+</body></html>
