be272f2fbd41f75dcd78a3c505b6ae7c49efcc68
HMSI <HMSI@GKV.LOCAL>
2018-04-20 10:54:32 +0700

    Lan 4

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $disallowed_extensions = [];  // must be an array. Extensions disallowed to be u
 
 $hidden_extensions = []; // must be an array of lowercase file extensions. Extensions hidden in directory index
 
-$PASSWORD = '123459';  // Set the password, to access the file manager... (optional)
+$PASSWORD = '1234510';  // Set the password, to access the file manager... (optional)
 
 if($PASSWORD) {
 
